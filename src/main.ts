b8c92df7f7d692e6ba110fbb63add28b59b5dc3e
Varyen <mail@varyen.ru>
2024-09-22 19:30:33 +0300

    demo commit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+const ProgressBar = require('progress');
 
-const host = process.env.HOST ?? 'localhost';
+const host = process.env.HOST ?? '0.0.0.0';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  await new Promise((resolve) => {
+    const bar = new ProgressBar(':bar', { total: 50 });
+    const timer = setInterval(function () {
+      bar.tick();
+      if (bar.complete) {
+        console.log('\ncomplete\n');
+        clearInterval(timer);
+        resolve(true);
+      }
+    }, 100);
+  });
   res.send({ message: 'Hello API' });
 });
 
